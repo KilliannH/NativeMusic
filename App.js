bf9978ba9dcdffe636aed6badc52cc62c8ab0bda
KilliannH <killiannhervagault@gmail.com>
2020-12-10 13:46:40 +0100

    updated gitignore

---
+++ App.js
@@ -0,0 +1,46 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+import 'react-native-gesture-handler';
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
+import DetailsScreen from './screens/DetailsScreen';
+import PlayerScreen from './screens/PlayerScreen';
+import {exp} from 'react-native-reanimated';
+
+const Stack = createStackNavigator();
+const appName = 'Native Music';
+
+// You should go for React.PureComponent when you can satisfy any of the below conditions.
+//
+// State/Props should be an immutable object
+// State/Props should not have a hierarchy
+// You should call forceUpdate when data changes
+// If you are using React.PureComponent you should make sure all child components are also pure.
+
+// So if we have a component that has an imunitable kind on props (string message for example)
+// then making it a pure cmponent shall increase performances
+// cf.https://medium.com/better-programming/when-to-use-react-purecomponent-723f85738be1
+
+const App: () => React$Node = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home"
+                       screenOptions={{title: appName}}>
+        <Stack.Screen name="Home"
+                      component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="Player" component={PlayerScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ screens/HomeScreen.js
@@ -0,0 +1,67 @@
+import React, {useState, useEffect} from 'react';
+import {View, Text, FlatList, ActivityIndicator} from 'react-native';
+import * as dataService from '../services/DataService';
+import SongCard from '../components/songCard';
+
+import globalStyles from '../constants/globalStyles';
+
+const HomeScreen = ({navigation}) => {
+
+  const [songList, setSongList] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const componentDidMount = () => {
+    console.log('did mount');
+    dataService.getAllSongs().then((songs) => {
+      setSongList(songs);
+      setLoading(false);
+    }).catch((e) => console.error(e));
+  };
+
+  const componentWillUnmount = () => {
+    setSongList([]);
+    setLoading(true);
+  };
+
+  useEffect(() => {
+    console.log('use effect fired');
+    componentDidMount();
+    return (
+      componentWillUnmount()
+    );
+  }, []);
+  // if homeScreen were a nested Component and soundlist (the datasource) were a property of that component
+  // we would added [songList] as a dependency to tell React that we want to fire this hook only if songList had changed.
+
+  // here songList is not a property of our component so we don't have to specify it as a dependency
+  // + an empty array is the appropriate behavior to retrieve the componentDidMount / componentWillUnmount paradigm of a class based component.
+
+  if (!loading) {
+    return (
+      <>
+        <View style={{
+          flexDirection: "row",
+          height: 80,
+          padding: 20,
+        }}>
+          <Text style={globalStyles.titleText}>All Songs</Text>
+        </View>
+        <View style={{
+          flex: 1,
+          flexDirection: 'column',
+          justifyContent: 'flex-start',
+        }}>
+          <FlatList
+            data={songList}
+            renderItem={(data) => <SongCard song={data.item} navigation={navigation} />}
+            keyExtractor={(item) => item._id}
+          />
+        </View>
+      </>
+    );
+  } else {
+    return <ActivityIndicator />;
+  }
+};
+
+export default HomeScreen;
